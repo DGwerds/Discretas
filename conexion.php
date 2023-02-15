221e3107c7b2ba03afec7a3797a0ed7ee157a14d
Werds <santiagoandresxbox@gmail.com>
2023-02-15 17:05:08 -0500

    Mejoras y separacion

--- conexion.php
+++ conexion.php
@@ -6,9 +6,11 @@
 // }
 
 $conexion = pg_connect("host=localhost dbname=prueba user=postgres password=contraPostgre");
-// $query = "SELECT * FROM almacen.productos p Where p.cantidad = 0";
+pg_query($conexion, "SET lc_monetary = 'es_CO';");
 $query = "SELECT * FROM almacen.productos p Where p.id != -1";
-if (isset($_POST['submit'])) {
+// $query = "SELECT * FROM almacen.productos p Where p.cantidad = 0";
+$mostrar_agotados = 0;
+if (isset($_POST['buscar'])) {
 	$id = $_POST['id'];
 	if (!empty($id)) {
 		$query .= " and p.id = ".$id;
@@ -21,12 +23,12 @@ if (isset($_POST['submit'])) {
 
 	$categoria = $_POST['categoria'];
 	if (!empty($categoria)) {
-		$query .= " and p.categoria = '".$categoria."'";
+		$query .= " and p.categoria ILIKE '%".$categoria."%'";
 	}
 
 	$marca = $_POST['marca'];
 	if (!empty($marca)) {
-		$query .= " and p.marca = '".$marca."'";
+		$query .= " and p.marca ILIKE '%".$marca."%'";
 	}
 
 	$mostrar_agotados = isset($_POST['mostrar_agotados']);
@@ -36,8 +38,12 @@ if (isset($_POST['submit'])) {
 
 	$precio_min = $_POST['precio_min'];
 	$precio_max = $_POST['precio_max'];
-	if (!empty($precio_max)) {
+	if (!empty($precio_min) and !empty($precio_max)) {
 		$query .= " and p.precio BETWEEN '".$precio_min."' and '".$precio_max."'";
+	}else if(empty($precio_min) and !empty($precio_max)){
+		$query .= " and p.precio BETWEEN '0' and '".$precio_max."'";
+	}else if(empty($precio_min) and !empty($precio_max)){
+		$query .= " and p.precio > '".$precio_min."'";
 	}
 }
 $query .= " LIMIT 199"; 
--- index.php
+++ index.php
@@ -5,66 +5,66 @@ require 'conexion.php';
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
 
-	<title>Document</title>
+  <title>Document</title>
+
 </head>
 
 <body>
-	<br>
-	<form action="" method="POST">
-		<input type="text" name="id" placeholder="Buscar por id" value="<?php echo (isset($_POST['id'])) ? $_POST['id'] : '' ?>">
-		<input type="text" name="nombre" placeholder="Buscar por nombre" value="<?php echo (isset($_POST['nombre'])) ? $_POST['nombre'] : '' ?>">
-		<input type="text" name="categoria" placeholder="Buscar por categoria" value="<?php echo (isset($_POST['categoria'])) ? $_POST['categoria'] : '' ?>">
-		<input type="text" name="marca" placeholder="Buscar por marca" value="<?php echo (isset($_POST['marca'])) ? $_POST['marca'] : '' ?>">
-		
-		<div class="form-check">
-			<input class="form-check-input" type="checkbox" name="mostrar_agotados" <?php if ($mostrar_agotados == "1") {
-																						echo "checked";
-																					} ?>>
-			<label class="form-check-label">Mostrar agotados</label>
-		</div>
-		<input type="text" name="precio_min" placeholder="precio minimo" value="<?php echo (isset($_POST['precio_min'])) ? $_POST['precio_min'] : '' ?>">
-		<input type="text" name="precio_max" placeholder="precio maximo" value="<?php echo (isset($_POST['precio_max'])) ? $_POST['precio_max'] : '' ?>">
-		<input type="submit" name="submit" value="Buscar">
-		<br>
-		<br>
-		<table class="table table-striped">
-			<thead class="thead-dark">
-				<tr>
-					<th>id</th>
-					<th>Nombre</th>
-					<th>categoria</th>
-					<th>cantidad</th>
-					<th>marca</th>
-					<th>precio</th>
-				</tr>
-			</thead>
+  <br>
+  <form action="" method="POST">
+    <input type="text" name="id" placeholder="Buscar por id" value="<?php echo (isset($_POST['id'])) ? $_POST['id'] : '' ?>">
+    <input type="text" name="nombre" placeholder="Buscar por nombre" value="<?php echo (isset($_POST['nombre'])) ? $_POST['nombre'] : '' ?>">
+    <input type="text" name="categoria" placeholder="Buscar por categoria" value="<?php echo (isset($_POST['categoria'])) ? $_POST['categoria'] : '' ?>">
+    <input type="text" name="marca" placeholder="Buscar por marca" value="<?php echo (isset($_POST['marca'])) ? $_POST['marca'] : '' ?>">
+    <input type="text" name="precio_min" placeholder="precio minimo" value="<?php echo (isset($_POST['precio_min'])) ? $_POST['precio_min'] : '' ?>"> -
+    <input type="text" name="precio_max" placeholder="precio maximo" value="<?php echo (isset($_POST['precio_max'])) ? $_POST['precio_max'] : '' ?>">
+    <br>
+    <input type="checkbox" name="mostrar_agotados" <?php if ($mostrar_agotados == "1") {
+                                                      echo "checked";
+                                                    } ?>> Mostrar agotados
+    <br>
+    <input type="submit" name="buscar" value="Buscar">
+    <!-- <input type="submit" name="agregar" value="agregar"> -->
+    <br>
+    <br>
+    <table class="table table-striped">
+      <thead class="thead-dark">
+        <tr>
+          <th>id</th>
+          <th>Nombre</th>
+          <th>categoria</th>
+          <th>cantidad</th>
+          <th>marca</th>
+          <th>precio</th>
+        </tr>
+      </thead>
 
-			<tbody>
-				<?php
-				while ($obj = pg_fetch_object($consulta)) {
-				?>
-					<tr>
-						<td><?php echo $obj->id; ?></td>
-						<td><?php echo $obj->nombre; ?></td>
-						<td><?php echo $obj->categoria; ?> </td>
-						<td><?php echo $obj->cantidad; ?> </td>
-						<td><?php echo $obj->marca; ?></td>
-						<td><?php echo $obj->precio; ?></td>
-					</tr>
-			</tbody>
-		<?php
-				}
-		?>
+      <tbody>
+        <?php
+        while ($obj = pg_fetch_object($consulta)) {
+        ?>
+          <tr>
+            <td><?php echo $obj->id; ?></td>
+            <td><?php echo $obj->nombre; ?></td>
+            <td><?php echo $obj->categoria; ?> </td>
+            <td><?php echo $obj->cantidad; ?> </td>
+            <td><?php echo $obj->marca; ?></td>
+            <td><?php echo $obj->precio; ?></td>
+          </tr>
+      </tbody>
+    <?php
+        }
+    ?>
 
-		</table>
+    </table>
 
-	</form>
+  </form>
 
 
 </body>
-
+<!-- <script src="index.js"></script> -->
 </html>
