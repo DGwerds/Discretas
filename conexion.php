d1ab2bf9032264b9d2b43f6abfa168d0d361270c
Werds <santiagoandresxbox@gmail.com>
2023-02-16 02:13:10 -0500

    Ultimo commit

--- conexion.php
+++ conexion.php
@@ -1,16 +1,52 @@
 <?php
-// if (array_key_exists('codigo', $_REQUEST)) {
-// 	$where = $_REQUEST['codigo'];
-// } else {
-// 	$where = '';
-// }
-
 $conexion = pg_connect("host=localhost dbname=prueba user=postgres password=contraPostgre");
 pg_query($conexion, "SET lc_monetary = 'es_CO';");
 $query = "SELECT * FROM almacen.productos p Where p.id != -1";
-// $query = "SELECT * FROM almacen.productos p Where p.cantidad = 0";
 $mostrar_agotados = 0;
-if (isset($_POST['buscar'])) {
+$mostrar_opciones = 0;
+$entrar = false;
+$eliminar = 0;
+$offset = $_POST['offset'];
+if (isset($_POST['adelante'])) {
+	$entrar = true;
+	$offset += 1;
+	$_POST['offset'] = $offset;
+}
+if (isset($_POST['atras'])) {
+	if ($offset >= 1){
+		$entrar = true;
+		$offset -= 1;
+		$_POST['offset'] = $offset;
+	}
+}
+
+if (isset($_POST['eliminar'])) {
+	$eliminar = $_POST['del'];
+	pg_query($conexion, "DELETE FROM almacen.productos p  WHERE p.id = ".$eliminar);
+}
+
+if (isset($_POST['agregar'])) {
+	$a_id = $_POST['a_id'];
+	$a_nombre = $_POST['a_nombre'];
+	$a_categoria = $_POST['a_categoria'];
+	$a_cantidad = $_POST['a_cantidad'];
+	$a_marca = $_POST['a_marca'];
+	$a_precio = $_POST['a_precio'];
+
+	if (!empty($a_nombre) and !empty($a_categoria) and !empty($a_cantidad) and !empty($a_marca) and !empty($a_precio)){
+		$id_aux = '';
+		$id_aux2 = '';
+		if (!empty($a_id)){
+			$id_aux .= 'id, ';
+			$id_aux2 .= $a_id.", ";
+		}
+		$agregar = "INSERT INTO almacen.productos (".$id_aux."nombre, categoria, cantidad, marca, precio)
+			VALUES (".$id_aux2."'$a_nombre', '$a_categoria', $a_cantidad, '$a_marca', $a_precio);";
+		pg_query($conexion, $agregar);
+	}
+}
+
+if (isset($_POST['buscar']) or $entrar) {
 	$id = $_POST['id'];
 	if (!empty($id)) {
 		$query .= " and p.id = ".$id;
@@ -36,6 +72,8 @@ if (isset($_POST['buscar'])) {
 		$query .= " and p.cantidad != 0";
 	}
 
+	$mostrar_opciones = isset($_POST['opciones_avanzadas']);
+
 	$precio_min = $_POST['precio_min'];
 	$precio_max = $_POST['precio_max'];
 	if (!empty($precio_min) and !empty($precio_max)) {
@@ -46,6 +84,13 @@ if (isset($_POST['buscar'])) {
 		$query .= " and p.precio > '".$precio_min."'";
 	}
 }
-$query .= " LIMIT 199"; 
+
+$query .= " order by p.id asc LIMIT 10 offset ".$offset*10;
+
 $consulta = pg_query($conexion, $query);
+if (!$consulta){
+	$consulta = pg_query($conexion, "SELECT * FROM almacen.productos p Where p.id != -1 
+	order by p.id asc LIMIT 10");
+	echo "Revisa los parametros de entrada porfavor";
+}
 ?>
--- index.php
+++ index.php
@@ -10,61 +10,117 @@ require 'conexion.php';
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
 
   <title>Document</title>
+  <style>
+    body {
+      background-color: #D3DFFF;
+    }
 
+    .peq {
+      max-width: 70px;
+      text-align: center;
+    }
+
+    .container mx-auto {
+      margin-top: 1%;
+    }
+  </style>
 </head>
 
 <body>
   <br>
-  <form action="" method="POST">
-    <input type="text" name="id" placeholder="Buscar por id" value="<?php echo (isset($_POST['id'])) ? $_POST['id'] : '' ?>">
-    <input type="text" name="nombre" placeholder="Buscar por nombre" value="<?php echo (isset($_POST['nombre'])) ? $_POST['nombre'] : '' ?>">
-    <input type="text" name="categoria" placeholder="Buscar por categoria" value="<?php echo (isset($_POST['categoria'])) ? $_POST['categoria'] : '' ?>">
-    <input type="text" name="marca" placeholder="Buscar por marca" value="<?php echo (isset($_POST['marca'])) ? $_POST['marca'] : '' ?>">
-    <input type="text" name="precio_min" placeholder="precio minimo" value="<?php echo (isset($_POST['precio_min'])) ? $_POST['precio_min'] : '' ?>"> -
-    <input type="text" name="precio_max" placeholder="precio maximo" value="<?php echo (isset($_POST['precio_max'])) ? $_POST['precio_max'] : '' ?>">
-    <br>
-    <input type="checkbox" name="mostrar_agotados" <?php if ($mostrar_agotados == "1") {
-                                                      echo "checked";
-                                                    } ?>> Mostrar agotados
-    <br>
-    <input type="submit" name="buscar" value="Buscar">
-    <!-- <input type="submit" name="agregar" value="agregar"> -->
-    <br>
-    <br>
-    <table class="table table-striped">
-      <thead class="thead-dark">
-        <tr>
-          <th>id</th>
-          <th>Nombre</th>
-          <th>categoria</th>
-          <th>cantidad</th>
-          <th>marca</th>
-          <th>precio</th>
-        </tr>
-      </thead>
+  <div class="container mx-auto">
+    <div class="card shadow-lg">
+      <div class="card-body">
+        <form action="" method="POST">
+          <div class="card-body" style="background-color: #F2F2F2;">
+            <label for="buscar"> Buscar: </label>
+            <input type="text" class="peq" name="id" placeholder="id" value="<?php echo $_POST['id'] ?>">
+            <input type="text" name="nombre" placeholder="Nombre producto" value="<?php echo $_POST['nombre'] ?>">
+            <input type="text" name="categoria" placeholder="Categoria" value="<?php echo $_POST['categoria'] ?>">
+            <input type="text" name="marca" placeholder="Marca" value="<?php echo $_POST['marca'] ?>">
+            <label>Rango precios: </label>
+            <input type="text" class="peq" name="precio_min" placeholder="minimo" value="<?php echo $_POST['precio_min'] ?>"> -
+            <input type="text" class="peq" name="precio_max" placeholder="maximo" value="<?php echo $_POST['precio_max'] ?>">
+            <label for="Mostrar agotados">- Mostrar agotados:</label>
+            <input type="checkbox" name="mostrar_agotados" <?php if ($mostrar_agotados == "1") echo "checked"; ?>>
+            <input type="submit" class="btn btn-success" name="buscar" value="Buscar">
+          </div>
+          <!-- <input type="submit" name="agregar" value="agregar"> -->
+          <br>
+          <label>- mostrar opciones avanzadas</label>
+          <input type="checkbox" id="opciones" name="opciones_avanzadas" <?php if ($mostrar_opciones == "1") echo "checked";?>>
+          <div id="div_agregar" class="card-body" style="background-color: #F2F2F2; display: none;">
+            <input type="text" class="peq" name="a_id" placeholder="id" value="<?php echo $_POST['id'] ?>">
+            <input type="text" name="a_nombre" placeholder="Nombre producto" value="<?php echo $_POST['nombre'] ?>">
+            <input type="text" name="a_categoria" placeholder="Categoria" value="<?php echo $_POST['categoria'] ?>">
+            <input type="text" class="peq" name="a_cantidad" placeholder="cantidad" value="<?php echo $_POST['categoria'] ?>">
+            <input type="text" name="a_marca" placeholder="Marca" value="<?php echo $_POST['marca'] ?>">
+            <input type="text" class="peq" name="a_precio" placeholder="precio" value="<?php echo $_POST['precio_min'] ?>">
+            <input type="submit" class="btn btn-warning" name="agregar" value="Agregar">
+          </div>
+          <table class="table table-striped">
+            <thead class="thead-dark">
+              <tr>
+                <th>id</th>
+                <th>Nombre</th>
+                <th>categoria</th>
+                <th>cantidad</th>
+                <th>marca</th>
+                <th>precio</th>
+                <th></th>
+              </tr>
+            </thead>
 
-      <tbody>
-        <?php
-        while ($obj = pg_fetch_object($consulta)) {
-        ?>
-          <tr>
-            <td><?php echo $obj->id; ?></td>
-            <td><?php echo $obj->nombre; ?></td>
-            <td><?php echo $obj->categoria; ?> </td>
-            <td><?php echo $obj->cantidad; ?> </td>
-            <td><?php echo $obj->marca; ?></td>
-            <td><?php echo $obj->precio; ?></td>
-          </tr>
-      </tbody>
-    <?php
-        }
-    ?>
+            <tbody>
+              <?php
+              while ($obj = pg_fetch_object($consulta)) {
+              ?>
+                <tr>
+                  <td><?php echo $obj->id; ?></td>
+                  <td><?php echo $obj->nombre; ?></td>
+                  <td><?php echo $obj->categoria; ?> </td>
+                  <td><?php echo $obj->cantidad; ?> </td>
+                  <td><?php echo $obj->marca; ?></td>
+                  <td><?php echo $obj->precio; ?></td>
+                  <td><button type="submit" name="eliminar" class="btn btn-danger btn-sm" onclick=eliminarfila(this)>Eliminar</button></td>
+                </tr>
+            </tbody>
+          <?php
+              }
+          ?>
 
-    </table>
-
-  </form>
-
-
+          </table>
+          <div style="text-align: center;">
+            <input type="submit" name="atras" class="btn btn-info" value="anterior">
+            <input type="text" class="peq" name="offset" value="<?php echo $_POST['offset'] ?>">
+            <input type="submit" name="adelante" class="btn btn-info" value="siguiente">
+          </div>
+          <input id="del" name="del" style="display: none;">
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
 <!-- <script src="index.js"></script> -->
+<script>
+  const checkbox = document.getElementById('opciones');
+  checkbox.addEventListener("change", revisar);
+  revisar()
+  function revisar(){
+    const div = document.getElementById('div_agregar');
+    if (checkbox.checked) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+
+  function eliminarfila(boton) {
+    let fila = boton.parentNode.parentNode;
+    let id = fila.getElementsByTagName("td")[0];
+    document.getElementById("del").value = id.innerHTML;
+    console.log("<?php echo $eliminar ?>")
+  }
+</script>
+
 </html>
